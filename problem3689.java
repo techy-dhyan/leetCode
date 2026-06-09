3d6a0fb2fadad38c3aa03cfefed9bf1c40d383be
techy-dhyan <dhyanakash2206@gmail.com>
2026-06-09 10:17:43 +0530

    prb 3689

---
+++ problem3689.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = nums[0];
+        int min = nums[0];
+        for (int i=0;i<nums.length;i++){
+            if (nums[i] > max) max = nums[i];
+            if (nums[i] < min) min = nums[i];
+        }
+        long o = (long) k*(max - min);
+        return o;
+    }
+}
