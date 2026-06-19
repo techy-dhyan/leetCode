83a59d999750cfe5644f4204bacd19351bcd4806
techy-dhyan <dhyanakash2206@gmail.com>
2026-06-19 14:30:02 +0530

    Create problem561.java

---
+++ java/problem561.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i = 0;i<n;i+=2){
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
