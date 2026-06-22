148f0da566ac497fbfc77b2abacccea8aae4d179
techy-dhyan <dhyanakash2206@gmail.com>
2026-06-22 12:04:51 +0530

    Create problem349.java

---
+++ java/problem349.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i : nums1){
+            if (!map.containsKey(i)){
+                map.put(i,1);
+            }
+            else{
+                map.put(i,map.get(i) + 1);
+            }
+        }
+        for (int i : nums2){
+            if (map.containsKey(i)){
+                if (map.get(i)>=1){
+                    result.add(i);
+                    map.remove(i);
+                }
+            }
+        }
+        int[] finalArray = result.stream().mapToInt(i -> i).toArray();
+        return finalArray;
+    }
+}
